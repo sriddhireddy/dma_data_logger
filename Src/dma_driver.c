94281f6de897c8d9c4a156ccbcbe0d38f40048f5
S Riddhi Reddy <sriddhireddy20@gmail.com>
2026-08-12 00:11:39 +0530

    feat: clean DMA configuration and enable FIFO mode

--- Src/dma_driver.c
+++ Src/dma_driver.c
@@ -2,15 +2,35 @@
 
 #define DMA2EN			(1U<<22)
 #define DMA_CR_EN		(1U<<0)
-#define DMA_CR_CHSEL	(0U<<25) 	//default
-#define DMA_CR_DIR		(0U<<6) 	//default
+
+//#define DMA_CR_CHSEL	(0U<<25) 	//default
+
+#define DMA_CR_CHSEL_Pos    25U
+#define DMA_CR_CHSEL_Msk    (7U << DMA_CR_CHSEL_Pos)
+
+//#define DMA_CR_DIR		(0U<<6) 	//default
+#define DMA_CR_DIR_Pos      6U
+#define DMA_CR_DIR_Msk      (3U << DMA_CR_DIR_Pos)
+
 #define DMA_CR_MINC		(1U<<10)
 #define DMA_CR_PINC		(0U<<9)		//default
-#define DMA_CR_PSIZE	(1U<<11)
-#define DMA_CR_MSIZE	(1U<<13)
+//#define DMA_CR_PSIZE	(1U<<11)
+#define DMA_CR_PSIZE_Pos    11U
+#define DMA_CR_PSIZE_Msk    (3U << DMA_CR_PSIZE_Pos)
+
+//#define DMA_CR_MSIZE	(1U<<13)
+#define DMA_CR_MSIZE_Pos    13U
+#define DMA_CR_MSIZE_Msk    (3U << DMA_CR_MSIZE_Pos)
+
 #define DMA_CR_CIRC		(1U<<8)
 #define DMA_CR_TCIE		(1U<<4)
 #define DMA_CR_HTIE		(1U<<3)
+
+
+#define DMA_FCR_DMDIS_Pos		(2U)
+#define DMA_FCR_DMDIS_Msk		(1U << DMA_FCR_DMDIS_Pos)
+#define DMA_FCR_FTH_Pos			(0U)
+#define DMA_FCR_FTH_Msk			(3U << DMA_FCR_FTH_Pos)
 
 
 #define DMA_LIFCR_CTCIF		(1U<<5)
@@ -35,6 +55,8 @@ static volatile uint8_t dma_transfer_error = 0;
 static volatile uint8_t dma_direct_error = 0;
 static volatile uint8_t dma_fifo_error = 0;
 
+//NOTE: clear-set for multi bit and directly set for single bit
+
 void DMA2_Stream0_Init(uint32_t src, uint32_t dst, uint32_t len)
 {
     /* enable clk access to dma */
@@ -56,10 +78,14 @@ void DMA2_Stream0_Init(uint32_t src, uint32_t dst, uint32_t len)
 	DMA2_Stream0->NDTR = len;
 
     /* select stream0 channel0 */
-	DMA2_Stream0->CR |= DMA_CR_CHSEL;
+//	DMA2_Stream0->CR |= DMA_CR_CHSEL;
+	DMA2_Stream0->CR &= ~DMA_CR_CHSEL_Msk; //CLEAR
+	DMA2_Stream0->CR |= (0U << DMA_CR_CHSEL_Pos);; //SET
 
     /* configure peripheral-to-memory transfer direction */
-	DMA2_Stream0->CR |= DMA_CR_DIR;
+//	DMA2_Stream0->CR |= DMA_CR_DIR;
+	DMA2_Stream0->CR &= ~DMA_CR_DIR_Msk;
+	DMA2_Stream0->CR |= (0U << DMA_CR_DIR_Pos);
 
     /* enable memory increment */
 	DMA2_Stream0->CR |= DMA_CR_MINC;
@@ -68,16 +94,30 @@ void DMA2_Stream0_Init(uint32_t src, uint32_t dst, uint32_t len)
 	DMA2_Stream0->CR |= DMA_CR_PINC;
 
     /* configure peripheral data size as half-word */
-	DMA2_Stream0->CR |= DMA_CR_PSIZE;
+//	DMA2_Stream0->CR |= DMA_CR_PSIZE;
+	DMA2_Stream0->CR &= ~DMA_CR_PSIZE_Msk;
+	DMA2_Stream0->CR |= (1U << DMA_CR_PSIZE_Pos);
 
     /* configure memory data size as half-word */
-	DMA2_Stream0->CR |= DMA_CR_MSIZE;
+//	DMA2_Stream0->CR |= DMA_CR_MSIZE;
+	DMA2_Stream0->CR &= ~DMA_CR_MSIZE_Msk;
+	DMA2_Stream0->CR |= (1U << DMA_CR_MSIZE_Pos);
 
     /* enable circular mode */
 	DMA2_Stream0->CR |= DMA_CR_CIRC;
 
-    /* enable direct mode and disable FIFO */
-	DMA2_Stream0->FCR = 0;
+//    /* enable direct mode and disable FIFO */
+//	DMA2_Stream0->FCR = 0;
+
+
+	/* disable direct mode to enable FIFO mode */
+	DMA2_Stream0->FCR &= ~(DMA_FCR_DMDIS_Msk);
+	DMA2_Stream0->FCR |= (1U<<DMA_FCR_DMDIS_Pos);
+
+	/* configure FIFO threshold as 1/2 full */
+	DMA2_Stream0->FCR &= ~(DMA_FCR_FTH_Msk);
+	DMA2_Stream0->FCR |= (1U<<DMA_FCR_FTH_Pos);
+
 
     /* enable dma transfer complete interrupt */
 	DMA2_Stream0->CR |= DMA_CR_TCIE;
@@ -90,6 +130,9 @@ void DMA2_Stream0_Init(uint32_t src, uint32_t dst, uint32_t len)
 
     /* enable dma2 stream0 interrupt in NVIC */
 	NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+
+
+
 
 }
 
